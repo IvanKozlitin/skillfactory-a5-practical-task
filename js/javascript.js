56a931595d046e57e14af53bd68463595643a21f
KI-Shadow <ivankozlitin2012@gmail.com>
2020-09-10 12:30:29 +0300

    Change 3 files

---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+let var1 = "{var1}";
+let var2 = "{var2}";
+let var3 = "{var3}";
+let var4 = "{var4}";
+let var5 = "{var5}";
+let var6 = "{var6}";
+let speach = "{speach}";
+const verse = `{"text":["Жили-были ${var1} да ${var2}","Была у них ${var3}","Снесла ${var3} ${var4}, не простое - золотое","- ${var1} бил, бил - не разбил","- ${var2} била, била - не разбила","${var5} бежала, ${var6} задела, ${var4} упало и разбилось.","${var1} плачет, ${var2} плачет, а ${var3} кудахчет:","${speach}"]}`;
+// console.log(verse);
